83a8a56e373e4466667acb65685bc5b3e0d7a9bc
Emerick Rogul <erogul@brave.com>
2022-02-09 15:42:25 -0500

    Remove ALLOW_UNUSED macros
    Chromium change:
    
    https://source.chromium.org/chromium/chromium/src/+/44d8af458bb86ca8477493cef5bc9d1ed12a8f40
    
    commit 44d8af458bb86ca8477493cef5bc9d1ed12a8f40
    Author: Avi Drissman <avi@chromium.org>
    Date:   Tue Jan 25 19:08:20 2022 +0000
    
        Remove ALLOW_UNUSED macros
    
        This removes ALLOW_UNUSED_LOCAL, ALLOW_UNUSED_TYPE, and
        ANALYZER_ALLOW_UNUSED.
    
        Bug: 1286390

--- components/brave_wallet/renderer/brave_wallet_js_handler.cc
+++ components/brave_wallet/renderer/brave_wallet_js_handler.cc
@@ -6,6 +6,7 @@
 #include "brave/components/brave_wallet/renderer/brave_wallet_js_handler.h"
 
 #include <limits>
+#include <tuple>
 #include <utility>
 #include <vector>
 
@@ -208,7 +209,7 @@ void BraveWalletJSHandler::OnIsUnlocked(
   base::Value result = base::Value(!locked);
   v8::Local<v8::Value> local_result =
       content::V8ValueConverter::Create()->ToV8Value(&result, context);
-  ALLOW_UNUSED_LOCAL(resolver->Resolve(context, local_result));
+  std::ignore = resolver->Resolve(context, local_result);
 }
 
 void BraveWalletJSHandler::OnGetAllowedAccounts(
@@ -398,9 +399,9 @@ void BraveWalletJSHandler::SendResponse(
 
   v8::Local<v8::Promise::Resolver> resolver = promise_resolver.Get(isolate);
   if (success) {
-    ALLOW_UNUSED_LOCAL(resolver->Resolve(context, result));
+    std::ignore = resolver->Resolve(context, result);
   } else {
-    ALLOW_UNUSED_LOCAL(resolver->Reject(context, result));
+    std::ignore = resolver->Reject(context, result);
   }
 }
 
@@ -922,8 +923,7 @@ void BraveWalletJSHandler::SendAsync(gin::Arguments* args) {
         input, isolate->GetCurrentContext());
     if (base::JSONWriter::Write(*input_value, &input_json)) {
       std::string method;
-      ALLOW_UNUSED_LOCAL(
-          GetEthJsonRequestInfo(input_json, &id, nullptr, nullptr));
+      std::ignore = GetEthJsonRequestInfo(input_json, &id, nullptr, nullptr);
     }
 
     auto global_context(
